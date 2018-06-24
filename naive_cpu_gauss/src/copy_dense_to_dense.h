01d67560614ddc26e991fd77b795bc77705e6fb7
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2018-06-24 20:06:10 +0300

    bug fix

--- naive_cpu_gauss/src/copy_dense_to_dense.h
+++ naive_cpu_gauss/src/copy_dense_to_dense.h
@@ -5,10 +5,8 @@
 #include "matrix_utils.h"
 
 template<class Real>
-void copy_dense_to_dense(int batch_sz, int N, int M, Real *matrices_src, Real *&matrices_dst)
+void copy_dense_to_dense(int batch_sz, int N, int M, const Real *matrices_src, Real *matrices_dst)
 {
-    matrices_dst = new Real[batch_sz*N*M];
-
     for (int batch = 0;batch < batch_sz;batch++) {
         for (int row = 0;row < N;row++) {
             for (int col = 0;col < M;col++) {
